eae04d818a9a910e488f54ccc92cfab879f40b0a
hongjianqiang <569250030@qq.com>
2017-04-21 22:39:16 +0800

    看完第四章JavaScript图片库

--- JavaScript DOM编程艺术/第四章/scripts/showPic.js
+++ JavaScript DOM编程艺术/第四章/scripts/showPic.js
@@ -1,14 +1,27 @@
 (function() {
   'use strict';
 
-  setTimeout(function() {
-    function showPic(whichpic) {
-      var source = whichpic.getAttribute('href');
-      var placeholder = document.getElementById('placeholder');
+  function showPic(whichpic) {
+    var source = whichpic.getAttribute('href');
+    var placeholder = document.getElementById('placeholder');
+    var text = whichpic.getAttribute('title');
+    var description = document.getElementById('description');
 
-      placeholder.setAttribute('src', source);
-    }
+    placeholder.setAttribute('src', source);
+    description.childNodes[0].nodeValue = text;
+    console.log(description.firstChild.nodeValue);  // 与上面 description.childNodes[0].nodeValue 等价
+  }
 
-    window.showPic = showPic;
-  }, 10*1000);
+  window.showPic = showPic;
+
+  // 4.4 对这个函数进行扩展
+  function countBodyChildren() {
+    var body_element = document.getElementsByTagName('body')[0];
+
+    console.log(body_element.childNodes);
+    console.log(body_element.childNodes.length);
+    console.log(body_element.nodeType);
+  }
+  window.onload = countBodyChildren;
+
 })();
