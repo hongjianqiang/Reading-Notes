51f86dd4d8c8656e55a9f882842a414173661d22
hongjianqiang <569250030@qq.com>
2017-04-25 23:06:37 +0800

    最小优先队列

---
+++ 学习JavaScript数据结构与算法/第四章/PriorityQueue.js
@@ -0,0 +1,61 @@
+(function() {
+  'use strict';
+
+  /**
+   * 最小优先队列
+   */
+  function PriorityQueue() {
+    var items = [];
+
+    function QueueElement(element, priority) {
+      this.element = element;
+      this.priority = priority;
+    }
+
+    this.enqueue = function(element, priority) {
+      var queueElement = new QueueElement(element, priority);
+
+      if (this.isEmpty()) {
+        items.push(queueElement);
+      } else {
+        var added = false;
+        for (var i=0; i<items.length; i++) {
+          if (queueElement.priority < items[i].priority) {
+            items.splice(i, 0, queueElement);
+            added = true;
+            break;
+          }
+        }
+        if (!added) {
+          items.push(queueElement);
+        }
+      }
+    };
+
+    this.dequeue = function() {
+      return items.shift();
+    };
+
+    this.front = function() {
+      return items[0];
+    };
+
+    this.isEmpty = function() {
+      return items.length === 0;
+    };
+
+    this.clear = function() {
+      items = [];
+    };
+
+    this.size = function() {
+      return items.length;
+    };
+
+    this.print = function() {
+      console.log(JSON.stringify(items));
+    };
+  }
+
+  module.exports = PriorityQueue;
+})();
