4ad81c469c6f77eedb26e4197b10479913350bc5
洪建强 <hongjianqiang@hongjianqiangdeMacBook-Pro.local>
2017-08-29 23:55:18 +0800

    add Set.js

---
+++ 学习JavaScript数据结构与算法/第六章/Set.js
@@ -0,0 +1,45 @@
+(function() {
+    /**
+     * 集合
+     */
+
+    'use strict';
+
+    function Set() {
+        var items = {};
+
+        this.has = function(value) {
+            return items.hasOwnProperty(value);
+        };
+
+        this.add = function(value) {
+            if (!this.has(value)) {
+                items[value] = value;
+                return true;
+            }
+            return false;
+        };
+
+        this.remove = function(value) {
+            if (this.has(value)) {
+                delete items[value];
+                return true;
+            }
+            return false;
+        };
+
+        this.clear = function() {
+            items = {};
+        };
+
+        this.size = function() {
+            return Object.keys(items).length;
+        };
+
+        this.values = function() {
+            return Object.keys(items);
+        };
+    }
+
+    module.exports = Set;
+})();
