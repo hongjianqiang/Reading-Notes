e620d820ec29ef0f656f1944e7d0a4a77df0ba35
洪建强 <hongjianqiang@hongjianqiangdeMacBook-Pro.local>
2018-02-22 12:08:06 +0800

    学习了CSS揭秘

---
+++ CSS揭秘/modernizr-custom.js
@@ -0,0 +1,3 @@
+/*! modernizr 3.5.0 (Custom Build) | MIT *
+ * https://modernizr.com/download/?-applicationcache-setclasses !*/
+!function(n,e,o){var s=[],t={_version:"3.5.0",_config:{classPrefix:"",enableClasses:!0,enableJSClass:!0,usePrefixes:!0},_q:[],on:function(n,e){var o=this;setTimeout(function(){e(o[n])},0)},addTest:function(n,e,o){s.push({name:n,fn:e,options:o})},addAsyncTest:function(n){s.push({name:null,fn:n})}},Modernizr=function(){};Modernizr.prototype=t,Modernizr=new Modernizr;var a=e.documentElement;"svg"===a.nodeName.toLowerCase()}(window,document);
